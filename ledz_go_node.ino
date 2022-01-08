8462d5566dbed69e9bf9e561dbe4a8eccaa65775
jzhao1998 <46528670+jzhao1998@users.noreply.github.com>
2022-01-08 23:16:37 +0100

    Update ledz_go_node.ino

--- ledz_go_node.ino
+++ ledz_go_node.ino
@@ -1,12 +1,28 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
-//maintenant tout est en Serial et on va le remplacer après
+
+#include <ESP8266WebServer.h>
+#include <WiFiUdp.h>
+
+//String ssid = "ledzGO";
+//String psk = "password";
+String ssid = "HUAWEI P20 Pro";
+String psk = "19980927";
+
+WiFiUDP Udp;
+IPAddress Master_IP(192,168,43,81);
+IPAddress gateway(192,168,43,81);
+IPAddress subnet(255,255,255,0);
+IPAddress Broadcast(192,168,43,255);
+unsigned int localPort = 8888;
+
+
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE + 1]; //buffer to hold incoming packet,
+
+int nodeMode=1;
 void treatJson(DynamicJsonDocument json){
-  //exemple Command:{"Version":"1.0","Data":{"Type":"Command","Channels":[1,2,3,4]}}
-  //exemple Discovery:{"Version":"1.0","Data":{"Type":"Discovery"}
-  if(json["Data"]["Type"]=="Discovery"){
-      Serial.println("Discovery Mode");
-    }else if(json["Data"]["Type"]=="Command"){
+    //exemple Command:{"Version":"1.0","Data":{"Type":"Command","Channels":[1,2,3,4]}}
+    if(json["Data"]["Type"]=="Command"){
       Serial.println("Lum Mode");
       int RGB[]={json["Data"]["Channels"][0],json["Data"]["Channels"][1],json["Data"]["Channels"][2],json["Data"]["Channels"][3]};
       Serial.println(RGB[0]);
@@ -19,17 +35,38 @@ void treatJson(DynamicJsonDocument json){
 }
 void setup()
 {
-  Serial.begin(115200);  
+  Serial.begin(115200);
+  Serial.println("Begin"); 
+  
+  WiFi.begin(ssid, psk);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  Udp.begin(localPort);
 }
 
 void loop() {
-  //maintenant j'ai mis Serial mais ici ça devais remplacé par le socket
-  while(Serial.available()){
-    String read;
-    DynamicJsonDocument json(1024);
-    read=Serial.readString();
-    deserializeJson(json,read);
-    treatJson(json);
+  if(nodeMode==1){
+    Udp.beginPacket(Master_IP, localPort);
+    uint8_t macAddr[6];
+    WiFi.macAddress(macAddr);
+    char address[30];
+    Serial.println(WiFi.localIP().toString().c_str());
+    sprintf(address,"%s %s",WiFi.localIP().toString().c_str(),WiFi.macAddress().c_str());
+    Udp.println(address);
+    Udp.endPacket();
+    nodeMode=0;
+  }else{
+    int packetSize = Udp.parsePacket();
+    if (packetSize) {
+      Serial.println("RunMode");
+      int n = Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+      packetBuffer[n] = 0;
+      Serial.println(packetBuffer);
+      DynamicJsonDocument receivedData(1024);
+      deserializeJson(receivedData,packetBuffer);
+      treatJson(receivedData);
+    }
   }
-  
 }
